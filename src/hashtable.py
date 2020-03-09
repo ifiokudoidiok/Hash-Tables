cc4701a8fa754a954c6b7af3befd9390da3ca28a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-03-09 17:23:16 +0100

    insert and resize done

--- src/hashtable.py
+++ src/hashtable.py
@@ -46,12 +46,25 @@ class HashTable:
     def insert(self, key, value):
         '''
         Store the value with the given key.
+        
 
         Hash collisions should be handled with Linked List Chaining.
 
         Fill this in.
         '''
-        pass
+        hash_value = self._hash_mod(key)
+        
+        if self.storage[hash_value] is None:
+            self.storage[hash_value] = LinkedPair(key,value)
+            return
+        else:
+            curr_item = self.storage[hash_value]
+
+            while curr_item.next is not None:
+                curr_item = curr_item.next
+            curr_item.next = LinkedPair(key, value)
+            return
+        
 
 
 
@@ -74,6 +87,18 @@ class HashTable:
 
         Fill this in.
         '''
+        # Hash the key to get the correct location
+        hash_value = self._hash_mod(key)
+        # Check there is something stored at this index, if not, return None
+        if self.storage[hash_value] is None:
+            return None
+        else:
+        # set the temporary variable to be the first item in the linked list at the correct index
+        # Loop through checking if the current key matches the key provided
+        # if so, return the value
+        # once we reach the end of the chain, check the last value to see if it matches
+        # if it does, return the value
+        # if not, return None
         pass
 
 
@@ -84,16 +109,40 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        new_storage = [None] * self.capacity
+        for i in range(self.capacity):
+            new_storage[i] = self.storage[i]
+        self.storage = new_storage
 
+print('Hash Dammy:')
+print(hash('dammy') % 6)
 
+ht = HashTable(2)
+
+ht.insert("line_1", "Tiny hash table")
+ht.insert("line_2", "Filled beyond capacity")
+ht.insert("line_3", "Linked list saves the day!")
+print('------------------')
+print(ht)
+print('------------------')
 
 if __name__ == "__main__":
-    ht = HashTable(2)
+    ht = HashTable(7)
 
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
     ht.insert("line_3", "Linked list saves the day!")
+    ht.insert("key-0", "val-0")
+    ht.insert("key-1", "val-1")
+    ht.insert("key-2", "val-2")
+    ht.insert("key-3", "val-3")
+    ht.insert("key-4", "val-4")
+    ht.insert("key-5", "val-5")
+    ht.insert("key-6", "val-6")
+    ht.insert("key-7", "val-7")
+    ht.insert("key-8", "val-8")
+    ht.insert("key-9", "val-9")
 
     print("")
 
